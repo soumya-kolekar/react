cc46ca58d936702609b0b238554724119e398c60
soumya kolekar <80325786+soumya-kolekar@users.noreply.github.com>
2025-01-25 22:09:02 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { Toaster } from './components/ui/toaster.jsx'
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/react">
   <Provider store={store}>
   <App />
   <Toaster />
