4bd9a27c3441632a82b97d298d9daa2e0c6edd7a
soumya kolekar <80325786+soumya-kolekar@users.noreply.github.com>
2025-01-26 17:00:03 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',  // Vite's default output directory
+  },
   plugins: [react()],
   resolve: {
     alias: {
